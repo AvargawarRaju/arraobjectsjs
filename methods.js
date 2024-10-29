8a90068ac4ef6951e4ec261d0651fb89e723c117
AwargawarRaju <aurgyaraju@gmail.com>
2024-10-29 11:38:59 +0530

    added

---
+++ methods.js
@@ -0,0 +1,20 @@
+var arr=[1,2,3,]  // [1,2,3,4]
+ arr.push(4)
+ console.log(arr)
+
+var arr1=[1,2,3,4] //[1,2,3]
+arr1.pop()
+console.log(arr1)
+
+var arr2=[2,3,4]  //[1,2,3,4]
+arr2.unshift(1);
+console.log(arr2)
+
+var arr3=[1,2,3,4]  //[2,3,4]
+arr3.shift(3);
+console.log(arr3)
+
+
+
+var arr4=[1,2,3]  //"123"
+console.log(arr4.join(""))
